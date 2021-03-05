3af7c6eee00e656a9210b08a32b26e6a99c582d0
szymoneg <szymon.zsp5@gmail.com>
2021-03-05 18:38:56 +0100

    img picker

--- ViewController.swift
+++ ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var mealLabel: UILabel!
+    @IBOutlet weak var textFieldMeal: UITextField!
+    @IBOutlet weak var viewImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("XDDD")
+        textFieldMeal.delegate = self
     }
-
-
+    
+    @IBAction func setName(_ sender: UIButton) {
+        mealLabel.text = textFieldMeal.text
+    }
+    
+    @IBAction func tapImage(_ sender: Any) {
+        textFieldMeal.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.originalImage.rawValue)] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        viewImage.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
