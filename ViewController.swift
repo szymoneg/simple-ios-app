4d4af6deae5975b6e79928551813c70449c0e769
szymoneg <szymon.zsp5@gmail.com>
2021-03-05 15:35:48 +0100

    init +view

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootTracker
+//
+//  Created by Szymon Biliński on 05/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("XDDD")
+    }
+
+
+}
+
