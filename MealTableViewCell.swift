c3ad138020fba1cfd9afe4e054e9e667229ace79
szymoneg <szymon.zsp5@gmail.com>
2021-03-12 23:03:46 +0100

    add new view

---
+++ MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  FootTracker
+//
+//  Created by Szymon Biliński on 12/03/2021.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
