b0a66b9d2401ef5288254aed0eb08a152e5f0db5
szymoneg <szymon.zsp5@gmail.com>
2021-03-19 21:54:48 +0100

    style cell, sth todo

--- MealTableViewCell.swift
+++ MealTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class MealTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
